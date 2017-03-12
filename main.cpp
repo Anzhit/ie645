a9beeca904d051d39124998d9c20d9cece2eb952
Anchit <anchit.post@gmail.com>
2017-03-12 15:35:22 +0530

    Branching logic added

--- main.cpp
+++ main.cpp
@@ -1,16 +1,113 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void mprint(vector<vector<int>> vec);
+void mprint(vector<vector<double>> vec);
+double reduce(vector<vector<double>> &C);
 class Node{
 public:
-	unsigned int serial; //serial no.
-	int lb;  // lower bound
-	vector<vector<int>> C; // reduced cost square matrix
+	unsigned int serial=0; //serial no.
+	double lb=0;  // lower bound
+	vector<vector<double>> C; // reduced cost square matrix
 	vector<pair<int,int>> in; // indexed list of in arcs
 	vector<pair<int,int>> out; // indexed list of out arcs
-    vector<vector<pair<int,int>>> chains; // indexed list of directed chains
+    vector<list<int>> chains; // indexed list of directed chains
+    double theta(int I,int J){
+    	double ret=0;
+    	double min =DBL_MAX;
+    	for(int i=1;i<C.size();i++)
+    		if(min>C[I][i]&&i!=J)
+    			min=C[I][i];
+    	ret=min;
+    	min =DBL_MAX;
+    	for(int i=1;i<C.size();i++)
+    		if(min>C[i][J]&&i!=I)
+    			min=C[i][J];
+    	return ret+min;
+    }
+    pair<int,int> branchOn(){
+    	double max=DBL_MIN;
+    	pair<int,int> ret;
+    	for(int i=1;i<C.size();i++)
+    		for(int j=1;j<C.size();j++)
+    			if(C[i][j]==0&&theta(i,j)>max){
+    				max=theta(i,j);
+    				ret=make_pair(C[i][0],C[0][j]);
+    			}
+    	return ret;
+    }
+    Node branch1(int I,int  J){ // branch with arc (I,J)
+        Node r = *this;
+    	for(int i=0;r.C.size();i++) //SEG Fault Here
+    		r.C[i].erase(r.C[i].begin()+J); // remove column 
+    	r.C.erase(r.C.begin()+I); // remove row
+    	r.in.push_back(make_pair(I,J));
+    	//add to chain/s
+    	int front=C.size(),back=C.size();
+    	for(int i=0;i<r.chains.size();i++){
+    		if(r.chains[i].back()==I){
+    			r.chains[i].push_back(J);
+    			back=i;
+    		}
+    		if(r.chains[i].front()==J){
+    			r.chains[i].push_front(I);
+    			front=i;
+    		}
+    		
+    	}
+    	//try and connect chains
+    	if(front==C.size()&&back==C.size()){
+    		list<int> t; t.push_back(I);t.push_back(J);
+    		r.chains.push_back(t);
+    		r.invalidate(J,I);
+    	}
+    	else if(front<C.size()&&back<C.size()){
+    		//connect front, back
+    		r.chains[front].pop_front();
+    		r.chains[back].pop_back();
+    		r.chains[back].splice(r.chains[back].end(),r.chains[front]);
+    		//invalidate edges
+    		r.invalidChain(r.chains[back]);
+    		r.chains.erase(r.chains.begin()+front);
 
+    	}
+    	else if(front<C.size()){
+    		r.invalidChain(r.chains[front]);
+
+    	}
+    	else if(back<C.size()){
+    		r.invalidChain(r.chains[back]);
+
+    	}
+    	r.serial++;
+    	r.lb+=reduce(r.C);
+    	return r;
+    }
+    Node branch2(int I,int  J){ //branch w/o arc (I,J)
+    	Node r = *this;
+    	r.C[I][J]=DBL_MAX;
+    	r.lb+=reduce(r.C);
+    	r.out.push_back(make_pair(I,J));
+    	r.serial+=2;
+    	return r;
+    }
+    void invalidate(int I,int J){//invalidate arc I, J i.e find element corersponding to it in matrice and set to INF
+    	int l=C.size(),m=C.size();
+    	for(int i=0;i<C.size();i++)
+    		if(C[i][0]==I)
+    			l=i;
+    	for(int i=0;i<C.size();i++)
+    		if(C[0][i]==J)
+    			m=i;
+    	if(l<C.size()&&m<C.size())
+    		C[l][m]=DBL_MAX;
+    }
+    void invalidChain(list<int> c){
+    	for ( list<int>::const_iterator i = c.end() ; i != c.begin() ; ){
+    		--i;
+    	for ( list<int>::const_iterator j = c.begin() ; j != i ; ++j )
+    		invalidate(*i,*j);    		
+		}	
+    }
     void print(){
 
     	cout<<"Serial Number: "<<serial<<endl;
@@ -28,38 +125,37 @@ public:
     	cout<<"Directed Chains: \n";
     	for(int i=0;i<chains.size();i++){
     		cout<<"Chain "<<i<<" :";
-    		for(int j=0;chains[i].size();j++)
-    			cout<<'('<<chains[i][j].first<<','<<chains[i][j].second<<") ";
+    		for ( list<int>::const_iterator j = chains[i].begin() ; j != chains[i].end() ; ++j )
+    			cout<<*j<<" ";
     		cout<<endl;
     	}
     	cout<<endl;
     }
 };
-
-int reduce(vector<vector<int>> &C){
+double reduce(vector<vector<double>> &C){
 	//reduce the cost matrix and return the obtained lower bound
-	int ret=0;
-	for(int i=0;i<C.size();i++){
-		int t=*min_element(C[i].begin(),C[i].end());
+	double ret=0;
+	for(int i=1;i<C.size();i++){
+		double t=*min_element(C[i].begin()+1,C[i].end());
 		ret+=t;
 		if(t>0)
-			for(int j=0;j<C[i].size();j++)
+			for(int j=1;j<C[i].size();j++)
 				C[i][j]-=t;
 	}
-	for(int i=0;i<C.size();i++){
-		int t=INT_MAX;
-		for(int j=0;j<C.size();j++)
+	for(int i=1;i<C.size();i++){
+		double t=DBL_MAX;
+		for(int j=1;j<C.size();j++)
 			if(t>C[j][i])
 				t=C[j][i];
 		ret+=t;
 		if(t>0)
-			for(int j=0;j<C.size();j++)
+			for(int j=1;j<C.size();j++)
 				C[j][i]-=t;
 	}			
 
 	return ret;
 }
-void mprint(vector<vector<int>> vec){ //print the matrix
+void mprint(vector<vector<double>> vec){ //print the matrix
 	for (int i = 0; i < vec.size(); i++){
     	for (int j = 0; j < vec[i].size(); j++)
     	{
@@ -71,9 +167,33 @@ void mprint(vector<vector<int>> vec){ //print the matrix
 
 int main(){
 list<Node> nodes; // list of sorted nodes (acc LB) to be explored
+	int n;
+	//Take Input and create init node
+	cin>>n;
+	Node init;
+	init.C.resize(n+1);
+	for(int i=0;i<=n;i++){
+		init.C[i].resize(n+1);
+		init.C[i][0]=i;
+		init.C[0][i]=i;
+	}
 
-	vector<vector<int>> C={{1,2,3},{4,5,6},{7,8,9}};
-	cout<<reduce(C)<<endl;
-	mprint(C);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++)
+			cin>>init.C[i+1][j+1];
+	}
+	init.lb=reduce(init.C);
+	nodes.push_back(init);
+	init.print();
+	while(true){
+		Node cur=nodes.front();
+		pair<int,int> arc=cur.branchOn();
+		Node X=cur.branch1(arc.first,arc.second);
+		X.print();
+		Node Xbar=cur.branch2(arc.first,arc.second);
+		Xbar.print();
+		break;
+	}
+
 	return 0;
 }
